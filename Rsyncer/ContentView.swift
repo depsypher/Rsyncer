37a6a05e053dcc14de3dc45415836974224967d4
Ray Vanderborght <voidstar@gmail.com>
2023-10-16 10:49:07 -0700

    Initial Commit

---
+++ Rsyncer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rsyncer
+//
+//  Created by Ray Vanderborght on 2023-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rsyncer/RsyncerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RsyncerApp.swift
+//  Rsyncer
+//
+//  Created by Ray Vanderborght on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct RsyncerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
